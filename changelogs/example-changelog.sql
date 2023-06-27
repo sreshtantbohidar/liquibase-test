c6ac804d8b5260e688279d955f5c9fc4e0f45b78
Sreshtant Bohidar <b.sreshtant@gmail.com>
2023-06-27 12:54:24 +0530

    Update example-changelog.sql

--- changelogs/example-changelog.sql
+++ changelogs/example-changelog.sql
@@ -1,1 +1,4 @@
+-- liquibase formatted sql
 
+-- changeset Sresht:1687849326569-1
+CREATE TABLE "person" ("id" INTEGER NOT NULL, "name" VARCHAR(50) NOT NULL, "city" VARCHAR(30), CONSTRAINT "person_pkey" PRIMARY KEY ("id"));
