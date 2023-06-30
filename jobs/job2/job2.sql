bb47402c36c7f6ea124efc3d5d2bd90eee9abc1a
Sreshtant Bohidar <b.sreshtant@gmail.com>
2023-06-30 16:17:42 +0530

    Update job2.sql

--- jobs/job2/job2.sql
+++ jobs/job2/job2.sql
@@ -1,5 +1,5 @@
 -- liquibase formatted sql
 
 -- changeset Sresht:job2_1 runOnChange:True
-DROP TABLE "person";
+DROP TABLE "company";
 CREATE TABLE "company" ("id" INTEGER NOT NULL, "name" VARCHAR(50) NOT NULL, "department" VARCHAR(30), CONSTRAINT "country_pkey" PRIMARY KEY ("id"));
