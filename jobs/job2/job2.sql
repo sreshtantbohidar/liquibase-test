0a76dc71d3cf3a4655b06375c1674afd758d1df9
Sreshtant Bohidar <b.sreshtant@gmail.com>
2023-06-30 16:42:03 +0530

    Update job2.sql

--- jobs/job2/job2.sql
+++ jobs/job2/job2.sql
@@ -3,3 +3,4 @@
 -- changeset Sresht:job2_1 runOnChange:True
 DROP TABLE IF EXISTS "company";
 CREATE TABLE "company" ("id" INTEGER NOT NULL, "type" VARCHAR(50) NOT NULL, "name" VARCHAR(50) NOT NULL, "department" VARCHAR(30), CONSTRAINT "country_pkey" PRIMARY KEY ("id"));
+INSERT INTO public.company (id, "type", "name", department) VALUES(0, 'Manufacturing', 'Soft Co.', 'accounts');
