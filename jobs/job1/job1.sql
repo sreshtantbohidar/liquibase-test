cbc06345d2784aec39807e0c5b7df56637710751
Sreshtant Bohidar <b.sreshtant@gmail.com>
2023-06-30 16:56:17 +0530

    Update job1.sql

--- jobs/job1/job1.sql
+++ jobs/job1/job1.sql
@@ -2,6 +2,6 @@
 
 -- changeset Sresht:job1_1 runOnChange:True
 DROP TABLE IF EXISTS "person";
-CREATE TABLE "person" ("id" INTEGER NOT NULL, "name" VARCHAR(50) NOT NULL, "city" VARCHAR(30), CONSTRAINT "person_pkey" PRIMARY KEY ("id"));
+CREATE TABLE "person" ("id" INTEGER NOT NULL, "name" VARCHAR(50) NOT NULL, "city" VARCHAR(30), CONSTRAINT "person_pkey" PRIMARY KEY ("id"), "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW());
 INSERT INTO "person" (id, "name", city) VALUES (0, 'John', 'California');
 
