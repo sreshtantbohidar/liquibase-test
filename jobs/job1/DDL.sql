904d3571574121a5f865767676c96ffde53e4204
Sreshtant Bohidar <b.sreshtant@gmail.com>
2023-07-10 18:18:57 +0530

    Update DDL.sql

--- jobs/job1/DDL.sql
+++ jobs/job1/DDL.sql
@@ -3,4 +3,4 @@
 -- changeset Sresht:job2_ddl_1 runOnChange:True
 CREATE TABLE "company" ("id" INTEGER NOT NULL, "type" VARCHAR(50) NOT NULL, "name" VARCHAR(50) NOT NULL, "department" VARCHAR(30), "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW());
 -- changeset Sresht:job2_ddl_2 runOnChange:True
-CREATE TABLE "project" ("id" INTEGER NOT NULL, "type" VARCHAR(50) NOT NULL, "name" VARCHAR(50) NOT NULL, "department" VARCHAR(30), "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW());
+CREATE TABLE "project" ("id" INTEGER NOT NULL, "type" VARCHAR(50) NOT NULL, "name" VARCHAR(50) NOT NULL, "company" VARCHAR(30), "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW());
