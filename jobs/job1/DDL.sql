ead7095907a7f7b9b637dc6e1192295629d706eb
Sreshtant Bohidar <b.sreshtant@gmail.com>
2023-07-10 18:11:26 +0530

    Update DDL.sql

--- jobs/job1/DDL.sql
+++ jobs/job1/DDL.sql
@@ -1,6 +1,6 @@
 -- liquibase formatted sql
 
 -- changeset Sresht:job2_ddl_1 runOnChange:True
-CREATE TABLE "company" ("id" INTEGER NOT NULL, "type" VARCHAR(50) NOT NULL, "name" VARCHAR(50) NOT NULL, "department" VARCHAR(30), CONSTRAINT "country_pkey" PRIMARY KEY ("id"), "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW());
+CREATE TABLE "company" ("id" INTEGER NOT NULL, "type" VARCHAR(50) NOT NULL, "name" VARCHAR(50) NOT NULL, "department" VARCHAR(30), "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW());
 -- changeset Sresht:job2_ddl_2 runOnChange:True
-CREATE TABLE "project" ("id" INTEGER NOT NULL, "type" VARCHAR(50) NOT NULL, "name" VARCHAR(50) NOT NULL, "department" VARCHAR(30), CONSTRAINT "cproject_pkey" PRIMARY KEY ("id"), "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW());
+CREATE TABLE "project" ("id" INTEGER NOT NULL, "type" VARCHAR(50) NOT NULL, "name" VARCHAR(50) NOT NULL, "department" VARCHAR(30), "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW());
