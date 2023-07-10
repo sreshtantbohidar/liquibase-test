5657c58c9a4756edbcdb205b9eec715ecb5cdfc9
Sreshtant Bohidar <b.sreshtant@gmail.com>
2023-07-10 16:42:58 +0530

    Update DDL.sql

--- jobs/job1/DDL.sql
+++ jobs/job1/DDL.sql
@@ -1,1 +1,5 @@
+-- liquibase formatted sql
 
+-- changeset Sresht:job2_1 runOnChange:True
+DROP TABLE IF EXISTS "company";
+CREATE TABLE "company" ("id" INTEGER NOT NULL, "type" VARCHAR(50) NOT NULL, "name" VARCHAR(50) NOT NULL, "department" VARCHAR(30), CONSTRAINT "country_pkey" PRIMARY KEY ("id"), "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW());
