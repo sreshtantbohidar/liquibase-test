5e6fe051101e8c1c0fa23b8abc46dd6ee2dbc055
Sreshtant Bohidar <b.sreshtant@gmail.com>
2023-07-10 16:53:09 +0530

    Update DDL.sql

--- jobs/job1/DDL.sql
+++ jobs/job1/DDL.sql
@@ -3,3 +3,6 @@
 -- changeset Sresht:job2_1 runOnChange:True
 DROP TABLE IF EXISTS "company";
 CREATE TABLE "company" ("id" INTEGER NOT NULL, "type" VARCHAR(50) NOT NULL, "name" VARCHAR(50) NOT NULL, "department" VARCHAR(30), CONSTRAINT "country_pkey" PRIMARY KEY ("id"), "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW());
+
+-- changeset Sresht:job2_2 runOnChange:True
+CREATE TABLE "company" ("id" INTEGER NOT NULL, "type" VARCHAR(50) NOT NULL, "name" VARCHAR(50) NOT NULL, "department" VARCHAR(30), CONSTRAINT "country_pkey" PRIMARY KEY ("id"), "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW());
