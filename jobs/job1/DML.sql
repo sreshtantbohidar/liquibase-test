adaba704eca66a28e7f5ee06b2718ae1dda11175
Sreshtant Bohidar <b.sreshtant@gmail.com>
2023-07-10 18:28:56 +0530

    Update DML.sql

--- jobs/job1/DML.sql
+++ jobs/job1/DML.sql
@@ -6,3 +6,5 @@ INSERT INTO public.company (id, "type", "name", department) VALUES(0, 'Manufactu
 INSERT INTO public.company (id, "type", "name", department) VALUES(1, 'Accounts', 'Soft Co.', 'account');
 -- changeset Sresht:job1_dml_3 runOnChange:True
 INSERT INTO public.company (id, "type", "name", department) VALUES(2, 'IT Service', 'Soft Co.', 'accoun');
+-- changeset Sresht:job1_dml_4 runOnChange:True
+INSERT INTO public.company (id, "type", "name", department) VALUES(3, 'IT Service', 'Soft Co.', 'accoun');
