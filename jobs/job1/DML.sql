e7961e1d94b2eb8b171360cffa2ce2a963fd9923
Sreshtant Bohidar <b.sreshtant@gmail.com>
2023-07-10 18:01:51 +0530

    Update DML.sql

--- jobs/job1/DML.sql
+++ jobs/job1/DML.sql
@@ -1,4 +1,8 @@
 -- liquibase formatted sql
 
--- changeset Sresht:job1_1 runOnChange:True
+-- changeset Sresht:job1_dml_1 runOnChange:True
 INSERT INTO public.company (id, "type", "name", department) VALUES(0, 'Manufacturing', 'Soft Co.', 'accounts');
+-- changeset Sresht:job1_dml_2 runOnChange:True
+INSERT INTO public.company (id, "type", "name", department) VALUES(1, 'Accounts', 'Soft Co.', 'accounts');
+-- changeset Sresht:job1_dml_3 runOnChange:True
+INSERT INTO public.company (id, "type", "name", department) VALUES(2, 'IT Service', 'Soft Co.', 'accounts');
