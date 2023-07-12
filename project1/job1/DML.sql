51465d737d0805b4dd65ec06d80d89946a2a1adf
Sreshtant Bohidar <b.sreshtant@gmail.com>
2023-07-12 14:28:02 +0530

    Update DML.sql

--- project1/job1/DML.sql
+++ project1/job1/DML.sql
@@ -1,1 +1,10 @@
+-- liquibase formatted sql
 
+-- changeset Sresht:project1_job1_dml_1 runOnChange:True
+INSERT INTO public.company1 (id, "type", "name") VALUES(1, 'Manufacturing', 'Soft Co.');
+-- changeset Sresht:job1_dml_2 runOnChange:True
+INSERT INTO public.company1 (id, "type", "name") VALUES(2, 'Manufacturing', 'Soft Co.');
+-- changeset Sresht:project1_job1_dml_3 runOnChange:True
+INSERT INTO public.person1 (id, "type", "name") VALUES(1, 'cat1', 'Harry');
+-- changeset Sresht:project1_job1_dml_4 runOnChange:True
+INSERT INTO public.person2 (id, "type", "name") VALUES(2, 'cat2', 'John');
