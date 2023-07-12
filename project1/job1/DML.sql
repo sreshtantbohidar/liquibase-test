08e480803e9122ef4d2470bfc4c79023c8eaa328
Sreshtant Bohidar <b.sreshtant@gmail.com>
2023-07-12 15:21:36 +0530

    Update DML.sql

--- project1/job1/DML.sql
+++ project1/job1/DML.sql
@@ -2,7 +2,7 @@
 
 -- changeset Sresht:project1_job1_dml_1 runOnChange:True
 INSERT INTO public.company1 (id, "type", "name") VALUES(1, 'Manufacturing', 'Soft Co.');
--- changeset Sresht:job1_dml_2 runOnChange:True
+-- changeset Sresht:project1_job1_dml_2 runOnChange:True
 INSERT INTO public.company1 (id, "type", "name") VALUES(2, 'Manufacturing', 'Soft Co.');
 -- changeset Sresht:project1_job1_dml_3 runOnChange:True
 INSERT INTO public.person1 (id, "type", "name") VALUES(1, 'cat1', 'Harry');
