ae8a5bdb18e56bf9de10e040c13c78f3843218a7
Sreshtant Bohidar <b.sreshtant@gmail.com>
2023-07-12 14:22:05 +0530

    Update DDL.sql

--- project1/job1/DDL.sql
+++ project1/job1/DDL.sql
@@ -1,1 +1,6 @@
-..
+-- liquibase formatted sql
+
+-- changeset Sresht:project1_job1_ddl_1 runOnChange:True
+CREATE TABLE "company1" ("id" INTEGER NOT NULL, "type" VARCHAR(50) NOT NULL, "name" VARCHAR(50) NOT NULL, "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW());
+-- changeset Sresht:project1_job1_ddl_1 runOnChange:True
+CREATE TABLE "project1" ("id" INTEGER NOT NULL, "type" VARCHAR(50) NOT NULL, "name" VARCHAR(50) NOT NULL, "created_at" TIMESTAMPTZ NOT NULL DEFAULT NOW());
